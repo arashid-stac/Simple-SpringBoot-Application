901a6dcd0d139971337df0e34b86eaa726e09ea8
Alli Rashid <allirashid97@gmail.com>
2023-03-04 20:55:23 -0400

    Created JSON objects

--- src/main/java/com/sbtutorial/Main.java
+++ src/main/java/com/sbtutorial/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -13,7 +15,20 @@ public class Main {
     }
 
     @GetMapping("/greet")
-    public String greet(){
-        return "Hello";
+    public Greeting greet(){
+        Person alli = new Person("Alli", 25, 1000.00);
+        Greeting response = new Greeting(
+                "Hello",
+                List.of("Java", "Python", "Javascript"),
+                alli
+        );
+        return response;
+    }
+
+    record Person(String name, int age, double savings) {
+
+    }
+    record Greeting(String greet, List<String> faveProgrammingLanguages, Person person){
+
     }
 }
